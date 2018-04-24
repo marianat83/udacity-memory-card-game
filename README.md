5b86b6d0b0b8aa1e04a72c7a262751d31fdc64d2
Mariana Terrazas <seiko.mariana@gmail.com>
2018-04-24 18:41:38 -0500

    Memory Card game
    First version for revision for the udacity front end developer course

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# udacity-memory-card-game
+# Udacity Memory game
+
+An Udacity Project for the Front End Developer course.
+
+## Instructions to play
+
+Select one box to flip the card, if two filped card match you found a pair! 
+
+If not keep trying! the fewer steps you make the better! Have fun!
+
+# Click here to play
+
+Download the project or Click [this link](https://github.com/marianat83/memorycard)
--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-/*
- * Create a list that holds all of your cards
- */
+//select elements
+const deck = $('.deck');
+const restart = $('.restart');
+const replay = $('#play-again');
+const notplay = $('#close-popup');
+let openCardList = [];
+let moves = 0;
+let matchFound = 0;
+let timer;
+let enabled = true;
 
+//List of cards
+let cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-birthday-cake', 'fa fa-bolt', 'fa fa-smile-o', 'fa fa-birthday-cake', 'fa fa-pagelines', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-gamepad', 'fa fa-pagelines', 'fa fa-gamepad', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-smile-o']
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -21,18 +22,153 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+//create cards
+function createCards() {
+	//create random cards each time
+	shuffle(cards);
+  	cards.forEach(card => {
+		deck.append(`<li class="card"><i class="${card}"></i></li>`)
+	});
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+function startGame() {
+	location.reload();
+}
+
+function showCard() {
+	//onclick event to the cards
+	$('.card').on('click', function() {
+		if ($(this).hasClass('show open')) {
+			return;
+		}
+	if (enabled) {
+		$(this).toggleClass('show open animated tada');
+		openCardList.push($(this));
+		isMatch();
+		}
+	})	
+}
+
+//check if the selected open cards match
+function isMatch() {
+	if (openCardList.length === 2) {
+		enabled = false;
+		//if they match
+		if (openCardList[0][0].innerHTML == openCardList[1][0].innerHTML) {
+		//add animation, and a class 'match' to change the color
+		$(openCardList[0][0]).addClass('match animated flip');
+		$(openCardList[1][0]).addClass('match animated flip');
+		//update matchFound and moves.
+		matchFound++;
+		moves++;
+		clearOpenCardList();
+		//stop timer when all matches are found.
+		setTimeout(stopTimer, 1200);
+		} else {
+			//if they don't match, add a class 'not-match' to change the color
+		$(openCardList[0][0]).addClass('not-match');
+		$(openCardList[1][0]).addClass('not-match');
+		//update moves;
+		moves++;
+		clearOpenCardList();
+		//and hide the card after 1200ms.
+		setTimeout(hideCard, 1200);
+		}		
+	}
+	updateMoves();
+} 
+
+//hide cards that don't match
+function hideCard() {
+	$('.card').each(function() {
+		if ($(this).hasClass('not-match')) {
+			$(this).attr('class', 'card');
+		}
+	})
+	enabled = true;
+}
+
+//clear cards list
+function clearOpenCardList() {
+	openCardList = [];
+} 
+
+//update numbers of moves
+function updateMoves() {
+	$('.moves').text(moves);
+	updateStar();
+}
+
+//rate the user score
+function updateStar() {
+	if (moves > 28 && moves <= 40) {
+		$('#one-star').remove();
+	} 
+	if (moves <= 28 && moves >18) {
+		$('#two-star').remove();
+	}
+}
+
+//start timer when the player start playing
+function startTimer() {
+	let count = 0;
+	$('.card').on('click', function() {
+		count++;
+		if (count === 1) {
+			var sec = 0;
+			function pad(val) {
+				return val > 9 ? val : "0" + val;
+			}
+			timer = setInterval(function () {
+				$('.seconds').html(pad(++sec % 60));
+				$('.minutes').html(pad(parseInt(sec / 60, 10)));
+			}, 1000);
+		}
+	})
+}
+
+//stop timer when game is over
+function stopTimer() {
+	enabled = true;
+	if (matchFound === 8) {
+		clearInterval(timer);
+		togglePopup();
+	}
+}
+
+//toggle popup window
+function togglePopup() {
+	$('#score-popup').popup('show');
+	replay.on('click', function() {
+		startGame();
+	})
+	notplay.on('click', function() {
+		$('#score-popup').popup('hide');
+	});
+}
+
+//overlay popup http://dev.vast.com/jquery-popup-overlay/
+$('#score-popup').popup({
+	color: 'white',
+	opacity: 0.96,
+	transition: '0.3s',
+	scrolllock: true
+  });
+
+$('#score-popup').css('text-align', 'center');
+
+//reset the game
+function restartGame() {
+	restart.on('click', function() {
+		startGame();
+	})
+}
+
+//call the functions
+createCards();
+showCard();
+startTimer();
+restartGame();
